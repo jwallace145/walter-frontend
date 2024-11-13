1221e29a886912784d21d2b8b6bcad9a1bdc833c
Jimmy Wallace <jimmy.wallace145@gmail.com>
2024-11-12 21:12:44 -0500

    fix: Fix loading for portfolio stock line chart widget (#27)

--- src/components/portfolio/linechart/PortfolioStockLineChartWidget.tsx
+++ src/components/portfolio/linechart/PortfolioStockLineChartWidget.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-import { Price } from '../../../api/GetPrices';
+import { GetPricesResponse, Price } from '../../../api/GetPrices';
 import { Container, Pagination } from '@mui/material';
 import PortfolioStockLineChart from './PortfolioStockLineChart';
 import { PortfolioStock } from '../../../api/GetPortfolio';
@@ -19,19 +19,22 @@ const PortfolioStockLineChartWidget: React.FC<
   const [page, setPage] = useState<number>(1);
   const [stock, setStock] = useState<string>('');
   const [prices, setPrices] = useState<Price[]>([]);
+  const [stockLoading, setStockLoading] = useState<boolean>(true);
 
   useEffect(() => {
     if (props.stocks !== undefined && props.stocks.length > 0) {
       setStock(props.stocks[page - 1].symbol);
-      WalterAPI.getPrices(props.stocks[page - 1].symbol).then((response) => {
-        setPrices(response.getPrices());
-      });
+      setStockLoading(true);
+      WalterAPI.getPrices(props.stocks[page - 1].symbol)
+        .then((response: GetPricesResponse) => setPrices(response.getPrices()))
+        .catch((error) => console.log(error))
+        .finally(() => setStockLoading(false));
     }
   }, [page, props.stocks]);
 
   return (
     <>
-      {props.loading ? (
+      { (props.loading || stockLoading) ? (
         <LoadingCircularProgress />
       ) : (
         <Container>
