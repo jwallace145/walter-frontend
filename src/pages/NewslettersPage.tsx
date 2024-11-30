9d84a4427f8cf5b47c4fe6532a8cbf125384e997
Jimmy Wallace <jimmy.wallace145@gmail.com>
2024-11-30 18:54:25 -0500

    feat: Add Unsubscribe component to Newsletters page

--- src/pages/NewslettersPage.tsx
+++ src/pages/NewslettersPage.tsx
@@ -1,10 +1,12 @@
 import SendNewsletter from '../components/newsletter/SendNewsletter';
 import Subscribe from '../components/subscribe/Subscribe';
+import Unsubscribe from '../components/unsubscribe/Unsubscribe';
 
 const NewslettersPage: React.FC = () => {
   return (
     <>
       <Subscribe />
+      <Unsubscribe />
       <SendNewsletter />
     </>
   );
