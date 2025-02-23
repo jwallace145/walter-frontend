687a954b1f86782e816fc5141775a8995cb54478
Jimmy Wallace <jimmy.wallace145@gmail.com>
2025-02-23 13:47:48 -0500

    feat: Mobile friendly login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,20 +2,50 @@ import React from 'react';
 import Grid from '@mui/material/Grid2';
 import LoginForm from '../components/login/LoginForm';
 import LoginInformational from '../components/login/LoginInformational';
+import useIsMobile from '../components/utils/isMobile';
 
 interface LoginPageProps {
   setAuthenticated: (authenticated: boolean) => void;
 }
 
-const LoginPage: React.FC<LoginPageProps> = (props) => {
-  return (
-    <Grid container direction="row">
-      <Grid size={6}>
-        <LoginInformational />
-      </Grid>
-      <Grid size={6}>
+const FULL_PAGE_WIDTH: number = 12;
+const HALF_PAGE_WIDTH: number = 6;
+
+const LoginPage: React.FC<LoginPageProps> = (props: LoginPageProps) => {
+  const isMobile: boolean = useIsMobile();
+
+  const getLoginPage = (isMobile: boolean) => {
+    if (isMobile) {
+      return getLoginForm(FULL_PAGE_WIDTH);
+    } else {
+      return (
+        <>
+          {getLoginInformational(HALF_PAGE_WIDTH)}
+          {getLoginForm(HALF_PAGE_WIDTH)}
+        </>
+      );
+    }
+  };
+
+  const getLoginForm = (size: number) => {
+    return (
+      <Grid size={size} sx={{ marginTop: isMobile ? '60px' : '120px' }}>
         <LoginForm setAuthenticated={props.setAuthenticated} />
       </Grid>
+    );
+  };
+
+  const getLoginInformational = (size: number) => {
+    return (
+      <Grid size={size}>
+        <LoginInformational />
+      </Grid>
+    );
+  };
+
+  return (
+    <Grid container direction="row">
+      {getLoginPage(isMobile)}
     </Grid>
   );
 };
